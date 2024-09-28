e504e0f2bf2a5f3129a78414bf8f565405d78d99
LennertJ <32568318+LennertJ@users.noreply.github.com>
2024-09-28 10:29:27 +0200

    Add files via upload

---
+++ naam_en_leeftijd.py
@@ -0,0 +1,6 @@
+def naam_en_leeftijd():
+    naam = input("Wat is jouw naam?")
+    leeftijd = input("Wat is jouw leeftijd?")
+    print("Jouw naam is", naam, "jouw leeftijd is", leeftijd,".")
+
+naam_en_leeftijd()
